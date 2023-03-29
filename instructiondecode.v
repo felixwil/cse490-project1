17d4b4921fc7c1e2a56c67b51dd33588e5d6ed94
felixwil <126042297+felixwil@users.noreply.github.com>
2023-03-29 13:44:07 -0400

    added instruction decoder

---
+++ instructiondecode.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/29/2023 11:36:33 AM
+// Design Name: 
+// Module Name: instructiondecode
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// IF ID EXE MEM WB
+module instructiondecode(
+        input [2:0] inst,
+        output registerwrite,
+        output aluop,
+        output alusrc,
+        output writeback,
+    );
+    parameter ADD   = 0;
+    parameter ADDI  = 1;
+    parameter SW    = 2;
+    parameter LW    = 3;
+    parameter SLL   = 4;
+
+    // if the instruction is not SW, we are writing back to regs
+    assign registerwrite = inst != SW;
+    // we are adding unless the instruction is SLL
+    assign aluop  = inst == SLL;
+    // only time immediate data goes right to the ALU is for ADDI
+    assign alusrc = inst == ADDI;
+    // write to mem if inst == SW
+    assign memw = inst == SW;
+endmodule
+
