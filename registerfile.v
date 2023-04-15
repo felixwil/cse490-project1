20b835fab629eb31db0dd88ed604bf18e35a1d31
holdenwozniak <71784655+holdenwozniak@users.noreply.github.com>
2023-04-15 14:17:35 -0400

    fixed registerfile.v to return register values

--- registerfile.v
+++ registerfile.v
@@ -22,23 +22,29 @@
 // IF ID EXE MEM WB
 module registerfile(
         input sysclk,
-        output reg [7:0] read0,
-        output reg [7:0] read1,
         input [7:0] w,
         input rw,
         input wsel,
-        input [1:0] rsel
+        input [1:0] rsel,
+        output reg [7:0] read0,
+        output reg [7:0] read1
     );
-    reg [7:0] A;
-    reg [7:0] B;
+    
+    reg [7:0] S0;
+    reg [7:0] S1;
+    
+    initial begin S0 <= 8'b00000000; end
+    initial begin S1 <= 8'b00000000; end
+
+    
     always @(posedge sysclk) begin
         if (rw == 0) begin // 0 == reading
-            #1 read0 <= (rsel[0]) ? read0 : read1;
-            #1 read1 <= (rsel[1]) ? read0 : read1;
+            begin read0 = (rsel[0]) ? S0 : S1; end
+            begin read1 = (rsel[1]) ? S0 : S1; end
         end
         else begin // 1 == writing
-            if (wsel == 0) begin #1 A <= w; end
-            else           begin #1 B <= w; end
+            if (wsel == 0) begin S0 = w; end
+            else           begin S1 = w; end
         end
     end
 endmodule
