8d5f20f3e3ba1357f7cfd1a4a741dd077e4e9f01
Colden Hoelscher <56481980+ColdenHoelscher@users.noreply.github.com>
2023-04-16 08:25:57 -0400

    Create multiplexer2.v

---
+++ multiplexer2.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/29/2023 12:33:33 PM
+// Design Name: 
+// Module Name: multiplexer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// IF ID EXE MEM WB
+module multiplexer2(
+        input [7:0] a,
+        input [7:0] b,
+        input sysclk,
+        input w, //if writing back to reg
+        input sel,
+        output reg [7:0] x
+    );
+    always @(posedge sysclk) begin
+        #6;
+        if(w) begin
+            assign x = (sel) ? a : b;
+        end
+    end
+endmodule
