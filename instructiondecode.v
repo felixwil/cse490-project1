5f6d9db5bceeb9554a6723e27bc4e0849e93e297
Colden Hoelscher <56481980+ColdenHoelscher@users.noreply.github.com>
2023-04-16 08:19:44 -0400

    Update instructiondecode.v

--- instructiondecode.v
+++ instructiondecode.v
@@ -21,29 +21,61 @@
 
 // IF ID EXE MEM WB
 module instructiondecode(
-        input sysclk,
         input [2:0] inst,
-        output registerwrite,
-        output aluop,
-        output alusrc,
-        output memw,
-        output reg2mem
+        input sysclk,
+        output reg registerwrite,
+        output reg aluop,
+        output reg alusrc,
+        output reg memw,
+        output reg reg2mem
     );
     parameter ADD   = 0;
-    parameter ADDI  = 1;
-    parameter SW    = 2;
-    parameter LW    = 3;
-    parameter SLL   = 4;
+    parameter ADDI  = 4;
+    parameter SW    = 5;
+    parameter LW    = 6;
+    parameter SLL   = 7;
 
-    // if the instruction is not SW, we are writing back to regs
-    assign registerwrite = inst != SW;
-    // we are adding unless the instruction is SLL
-    assign aluop  = inst == SLL;
-    // only time immediate data goes right to the ALU is for ADDI
-    assign alusrc = inst == ADDI;
-    // write to mem if inst == SW
-    assign memw = inst == SW;
-    // update regs from mem if inst == LW
-    assign reg2mem = inst == LW;
+    always @(posedge sysclk) begin
+        #1;
+        // if the instruction is not SW, we are writing back to regs
+        //assign registerwrite = inst != SW;
+        if (inst == SW) begin
+            assign registerwrite = 0;
+        end
+        else begin
+            assign registerwrite = 1;
+        end
+        // we are adding unless the instruction is SLL
+        //assign aluop  = inst == SLL;
+        if (inst == SLL) begin
+            assign aluop = 0;
+        end
+        else begin
+            assign aluop = 1;
+        end
+        // only time immediate data goes right to the ALU is for ADDI, SW,LW
+        //assign alusrc = inst == ADDI;
+        if (inst == ADDI || inst == SW || inst == LW) begin
+            assign alusrc = 0;
+        end
+        else begin
+            assign alusrc = 1;
+        end
+        // write to mem if inst == SW
+        //assign memw = inst == SW;
+        if (inst == SW) begin
+            assign memw = 1;
+        end
+        else begin
+            assign memw = 0;
+        end
+        // update regs from mem if inst == LW
+        //assign reg2mem = inst == LW;
+        if (inst == LW) begin
+            assign reg2mem = 1;
+        end
+        else begin
+            assign reg2mem = 0;
+        end
+    end
 endmodule
-
