c89dfeae645339afeefdcef16443bae6e42f8465
Colden Hoelscher <56481980+ColdenHoelscher@users.noreply.github.com>
2023-04-16 08:27:52 -0400

    Update registerfile.v

--- registerfile.v
+++ registerfile.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/29/2023 11:47:36 AM
-// Design Name: 
-// Module Name: registerfile
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-// IF ID EXE MEM WB
 module registerfile(
         input sysclk,
         input [7:0] w,
@@ -37,14 +15,11 @@ module registerfile(
     initial begin S1 <= 8'b00000000; end
 
     
-    always @(posedge sysclk) begin
-        if (rw == 0) begin // 0 == reading
-            begin read0 = (rsel[0]) ? S0 : S1; end
-            begin read1 = (rsel[1]) ? S0 : S1; end
-        end
-        else begin // 1 == writing
-            if (wsel == 0) begin S0 = w; end
-            else           begin S1 = w; end
-        end
+    always @(posedge sysclk, w) begin
+        #2;
+        begin read0 = (rsel[0]) ? S0 : S1; end
+        begin read1 = (rsel[1]) ? S0 : S1; end
+        if (wsel == 0) begin S0 = w; end
+        else           begin S1 = w; end
     end
 endmodule
